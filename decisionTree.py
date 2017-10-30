fd8e11c4a6b1ded74988d3d1063170593873f938
Ankit kotnala <32392686+ankitkotnala007@users.noreply.github.com>
2017-10-31 01:32:19 +0530

    Add files via upload

--- decisionTree.py
+++ decisionTree.py
@@ -3,6 +3,12 @@
 from sklearn.externals.six import StringIO
 import pydotplus
 dot_data = StringIO()
+tree.export_graphviz(clf,
+                        out_file=dot_data,
+                        feature_names=iris.feature_names,
+                        class_names=iris.target_names,
+                        filled=True, rounded=True,
+                        impurity=False)
 
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
 graph.write_pdf("iris.pdf")
