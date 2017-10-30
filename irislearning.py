66cc0994a68cd67994eeaf983a8ac7042870573e
Ankit kotnala <32392686+ankitkotnala007@users.noreply.github.com>
2017-10-31 01:30:30 +0530

    Add files via upload

---
+++ irislearning.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn import tree
+
+iris = load_iris()
+test_idx = [0,50,100]
+
+#testing data 
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+print(test_target) 
+print(clf.predict(test_data))
+
+print(test_data[2],test_target[1])
+print(iris.feature_names, iris.target_names)
+
+
+
+
