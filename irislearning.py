480dd0d042042c738871687cb05298812e056525
Ankit kotnala <32392686+ankitkotnala007@users.noreply.github.com>
2017-10-31 01:31:02 +0530

    Add files via upload

--- irislearning.py
+++ irislearning.py
@@ -5,12 +5,10 @@ from sklearn import tree
 iris = load_iris()
 test_idx = [0,50,100]
 
-#testing data 
-test_target = iris.target[test_idx]
-test_data = iris.data[test_idx]
+#training data 
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
 
-clf = tree.DecisionTreeClassifier()
-clf.fit(train_data, train_target)
 
 print(test_target) 
 print(clf.predict(test_data))
