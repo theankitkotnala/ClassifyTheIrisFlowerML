9fb1e536c3f9d187dbb1c2fbd9a84fbed1fea61f
Ankit kotnala <32392686+ankitkotnala007@users.noreply.github.com>
2017-10-31 01:29:40 +0530

    Add files via upload

--- iris.py
+++ iris.py
@@ -1,6 +1,10 @@
 from sklearn.datasets import load_iris
 
 iris = load_iris()
+print(iris.feature_names)
+print(iris.target_names)
+print(iris.data[0])
+print(iris.target[51])
 
 for i in range(len(iris.target)):
     print("example %d : label %s , feature %s" % (i,iris.target , iris.data[i]))
